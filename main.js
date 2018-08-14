d885c7957c795a52dbc211c410fbd062e300b847
Luc Berger <luc.richard94@yahoo.com>
2018-08-14 19:46:53 +0100

    Add 2 functions checking for the 1st & last index of checked boxes & checking it works

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-const inputs = document.querySelectorAll("input");
+const inputs = [].slice.call(document.querySelectorAll("input"));
 
 for (input of inputs) {
-    input.addEventListener("click", event => {
-        console.log(event.shiftKey);
+    input.addEventListener("click", function(event) {
+        console.log(inputs.findIndex(checkedBox));
+        console.log(findLastCheck(inputs));
     });
+}
+
+function checkedBox(element) {
+    return element.checked;
+}
+
+function findLastCheck(array) {
+    for (let i = array.length - 1; i > 0; i--){
+        if(array[i].checked){
+            return i;
+        }
+    }
 }
